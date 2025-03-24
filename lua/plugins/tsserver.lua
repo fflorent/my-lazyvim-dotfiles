3928868a8f72bba84f797a79b42463c89f41af36
fflorent <florent.git@zeteo.me>
2025-03-24 13:01:54 +0100

    LSP typescript: non-relative import modules

--- lua/plugins/tsserver.lua
+++ lua/plugins/tsserver.lua
@@ -5,7 +5,26 @@ return {
       servers = {
         tsserver = {
           root_dir = require('lspconfig').util.find_git_ancestor,
-        }
+          init_options = {
+            preferences = {
+              importModuleSpecifierPreference = "non-relative",
+            }
+          }
+        },
+        vtsls = {
+          settings = {
+            javascript = {
+              preferences = {
+                importModuleSpecifier = "non-relative",
+              },
+            },
+            typescript = {
+              preferences = {
+                importModuleSpecifier = "non-relative",
+              },
+            },
+          },
+        },
       }
     }
   }
